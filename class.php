f1b385b31ddfe8430afd9138db43b31bbedf27f1
Tat_Samnith <tatsamnith083@gmail.com>
2017-01-18 10:21:20 +0700

    new project

---
+++ class.php
@@ -0,0 +1,53 @@
+<?php
+class TimeLine{
+    public function Service(){
+
+        $service = array(
+            array('msg' => ' elements that are used to trigger in-page functionality (like collapsing content), rather than linking to new pages or sections within the current page, these links should be given a', 'date' => '2017-01-11'),
+            array('msg' => 'hello2', 'date' => '2017-01-10'),
+            array('msg' => 'I want to know more about PHP', 'date' => '2017-01-16'),
+            array('msg' => 'hello3', 'date' => '2017-01-09'),
+            array('msg' => 'hello4', 'date' => '2017-01-08'),
+            array('msg' => 'hello5', 'date' => '2017-01-07'),
+            array('msg' => 'hello6', 'date' => '2017-01-06'),
+            array('msg' => 'hello7', 'date' => '2017-01-05'),
+            array('msg' => 'hello8', 'date' => '2017-01-04'),
+            array('msg' => 'hello9', 'date' => '2017-01-03'),
+            array('msg' => 'hello10', 'date' => '2017-01-02'),
+            array('msg' => 'hello11', 'date' => '2017-01-01')
+        );
+        usort($service, function($a, $b) {
+            if($a['date']==$b['date']) return 0;
+            return $a['date'] < $b['date']?1:-1;
+        });
+        return $service;
+
+    }
+    public function getPage($page=''){
+        $service=$this->Service();
+        $totalData=count($service);
+        $perPage=5;
+        $totalPage = ceil($totalData / $perPage);
+        if ($page!='') {
+            $page = $page;
+            if ($page <= 0)
+                $page = 1;
+            if ($page >= $totalData)
+                $page = $totalPage;
+        }
+        else {
+            $page = 1;
+        }
+
+        $startPage=($page*5)-5;
+        $start = ($page - 1) * $perPage;
+        $limitService=array_slice($service,$startPage,$perPage);
+        return $data=array('service' => $limitService, 'page' => $page,'totalPage'=>$totalPage);
+
+
+
+
+    }
+
+}
+?>
---
+++ timeline.php
@@ -0,0 +1,90 @@
+<?php
+session_start();
+?>
+<html>
+    <head>
+        <title>
+            Search form timeline
+        </title>
+        <!--bootstrap-->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        <!-- css style-->
+        <link rel="stylesheet" href="style.css">
+        <!--font awesome-->
+        <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    </head>
+<body>
+<?php
+require_once ('class.php');
+$timeClass= new TimeLine();
+if(isset($_GET['page'])){
+    $servicePage=$timeClass->getPage($_GET['page']);
+}
+else{
+    $servicePage=$timeClass->getPage(1);
+
+}
+
+?>
+<div class="container">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="page-header">
+            <h1 id="timeline" align="center">Timeline</h1>
+        </div>
+        <ul class="timeline">
+            <?php
+            $sort = array();
+            $i=0;
+            foreach($servicePage['service'] as $k=>$v) {
+                    if($i%2==0){ ?>
+                        <li>
+                            <div class="timeline-badge"></div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4 class="timeline-title">Mussum ipsum cacilds</h4>
+                                </div>
+                                <div class="timeline-body">
+                                    <p> <?php echo  $sort['msg'][$k]=$v['msg'];?> </p>
+                                    <p align="right"><small class="text-muted"><i class="glyphicon glyphicon-time"></i> <?php echo $sort['date'][$k] = $v['date'];?></small></p>
+                                </div>
+                            </div>
+                        </li>
+                     <?php }
+                     else { ?>
+                         <li class="timeline-inverted">
+                             <div class="timeline-badge warning"></i></div>
+                             <div class="timeline-panel">
+                                 <div class="timeline-heading">
+                                     <h4 class="timeline-title">Mussum ipsum cacilds</h4>
+                                 </div>
+                                 <div class="timeline-body">
+                                     <p> <?php echo  $sort['msg'][$k]=$v['msg'];?> </p>
+                                     <div class="row">
+                                         <div class="col-md-6">
+                                             <button type="button" class="btn btn-info"><small>Read more</small></button>
+                                         </div>
+                                         <div class="col-md-6" align="right">
+                                             <small  class="text-muted"><i class="glyphicon glyphicon-time"></i> <?php echo $sort['date'][$k] = $v['date'];?></small>
+                                         </div>
+                                     </div>
+                                 </div>
+                             </div>
+                         </li>
+                <?php  }
+              $i++;
+            } ?>
+
+        </ul>
+    </div>
+    <div class="col-md-6" align="left">
+        <a href="timeline.php?page=<?php  if ($servicePage['page'] == 1) echo $servicePage['page'] + 1 - 1; else echo $servicePage['page'] - 1;?>" class="btn btn-info" <?php if ($servicePage['page'] == 1) echo 'disabled="disabled"';?> role="button"><i class="fa fa-chevron-circle-left" aria-hidden="true"></i> Previous </a>
+    </div>
+    <div class="col-md-6" align="right">
+        <a href="timeline.php?page=<?php  if ($servicePage['page'] == $servicePage['totalPage']) echo $servicePage['page'] + 1 - 1; else echo $servicePage['page'] + 1;?>" class="btn btn-success" <?php if ($servicePage['page'] == $servicePage['totalPage']) echo 'disabled="disabled"';?> role="button" >Next <i class="fa fa-chevron-circle-right" aria-hidden="true"></i></a>
+    </div>
+</div>
+<!-- file js bootstrap-->
+<script src="bootstrap/js/jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
